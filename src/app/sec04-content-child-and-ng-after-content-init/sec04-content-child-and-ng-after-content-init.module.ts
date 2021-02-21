7ba7a873b3379e55cbccf02c2bf6ccc7bcd12f32
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-20 19:15:27 -0500

    add - start of sec04 'ContentChild'

---
+++ src/app/sec04-content-child-and-ng-after-content-init/sec04-content-child-and-ng-after-content-init.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+
+
+@NgModule({
+  declarations: [AuthFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class Sec04ContentChildAndNgAfterContentInitModule { }
