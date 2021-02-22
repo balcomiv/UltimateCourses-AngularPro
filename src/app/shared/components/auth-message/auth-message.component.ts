4c129b299e9930292e0043b0f8d7cdfcd67111f3
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-22 08:35:26 -0500

    add - auth message component

---
+++ src/app/shared/components/auth-message/auth-message.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-message',
+  templateUrl: './auth-message.component.html',
+  styles: [
+  ]
+})
+export class AuthMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthRememberComponent } from '../sec03-projecting-and-binding-to-components/auth-form/auth-remember.component';
+import { AuthMessageComponent } from './components/auth-message/auth-message.component';
 
 @NgModule({
-  declarations: [AuthRememberComponent],
+  declarations: [AuthRememberComponent, AuthMessageComponent],
   imports: [CommonModule, FormsModule],
-  exports: [CommonModule, FormsModule, AuthRememberComponent],
+  exports: [
+    CommonModule,
+    FormsModule,
+    AuthRememberComponent,
+    AuthMessageComponent,
+  ],
 })
 export class SharedModule {}
