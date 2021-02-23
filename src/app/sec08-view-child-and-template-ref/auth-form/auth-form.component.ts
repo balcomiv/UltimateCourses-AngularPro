69ae412524952ef36e51f19ae7c7d8ba80c4db1c
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-23 07:58:06 -0500

    add - sec08 ViewChild && TemplateRef

---
+++ src/app/sec08-view-child-and-template-ref/auth-form/auth-form.component.ts
@@ -0,0 +1,70 @@
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthRememberComponent } from '../../sec03-projecting-and-binding-to-components/auth-form/auth-remember.component';
+import { AuthMessageComponent } from '../../shared/components/auth-message/auth-message.component';
+import { User } from '../../shared/user';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styles: [],
+})
+export class AuthFormComponent
+  implements OnInit, AfterViewInit, AfterContentInit {
+  showMessage = new Observable<boolean>();
+
+  @ViewChild(AuthMessageComponent, { static: true })
+  authMessage: AuthMessageComponent | null = null;
+
+  @ContentChild(AuthRememberComponent)
+  authRemember: AuthRememberComponent | null = null;
+
+  @Output() submitted = new EventEmitter<User>();
+
+  ngOnInit(): void {
+    //  Note: I can access ViewChild here because I made it { static: true }
+    //
+    if (this.authMessage) {
+      this.authMessage.days = 30;
+    }
+    console.log(this.authMessage);
+  }
+
+  ngAfterViewInit(): void {
+    //  Note: This will cause 'expression changed after checked' error
+    //
+    // if (this.authMessage) {
+    //   this.authMessage.days = 30;
+    // }
+    // console.log(this.authMessage);
+  }
+
+  ngAfterContentInit(): void {
+    //  console.log('===> ngAfterContentInit', this.authRemember);
+
+    if (this.authRemember) {
+      console.log(this.authRemember);
+      this.showMessage = this.authRemember.checked as Observable<boolean>;
+    }
+
+    //  Note: This is null, even though Todd shows it working. Static?
+    //
+    // if (this.authMessage) {
+    //   this.authMessage.days = 30;
+    // }
+    // console.log('Auth Message: ', this.authMessage);
+  }
+
+  onSubmit(value: User): void {
+    this.submitted.emit(value);
+  }
+}
