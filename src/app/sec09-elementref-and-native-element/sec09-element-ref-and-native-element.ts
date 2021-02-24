e70bad88ba375278053c95fc1ffaf5999a9350a9
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-24 05:09:39 -0500

    add - components for sec 09 ElementRef && NativeElement

---
+++ src/app/sec09-elementref-and-native-element/sec09-element-ref-and-native-element.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+@NgModule({
+  declarations: [AuthFormComponent],
+  imports: [SharedModule],
+  exports: [SharedModule, AuthFormComponent],
+})
+export class Sec09ElementRefAndNativeElement {}
