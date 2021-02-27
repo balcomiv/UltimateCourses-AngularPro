9be7e12b8fffdd455fb2eaaac5620b2610bea809
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-27 15:19:15 -0500

    add - component stack for sec17 passing context to dynamic ng-template

---
+++ src/app/sec17-passing-context-to-dynamic-ng-template/example/example.stories.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { moduleMetadata } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import { ExampleComponent } from './example.component';
+
+export default {
+  title: 'Ultimate Angular/17 Passing Context to Dynamic <ng-template>',
+  component: ExampleComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [ExampleComponent],
+      imports: [CommonModule, FormsModule],
+    }),
+  ],
+} as Meta;
+
+export const WithTemplate: Story = () => ({
+  template: `
+   <app-example></app-example>
+`,
+});
