b9f4b6ff29d0414643416279823c103ce23e30b9
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-27 07:37:48 -0500

    add - component stack for sec13 dynamic components and output subscriptions

---
+++ src/app/sec13-dynamic-component-and-output-subscriptions/dynamic-auth-form/auth-form.stories.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { action } from '@storybook/addon-actions';
+import { moduleMetadata } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import { User } from '../../shared/user';
+import { AuthFormComponent } from '../auth-form/auth-form.component';
+import { DynamicAuthFormComponent } from './dynamic-auth-form.component';
+
+export default {
+  title: 'Ultimate Angular/13 Dynamic Component && Output Subscriptions',
+  component: AuthFormComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [AuthFormComponent, DynamicAuthFormComponent],
+      imports: [CommonModule, FormsModule],
+    }),
+  ],
+} as Meta;
+
+export const Login: Story = () => ({
+  template: `
+      <div class="app-root">
+        <app-dynamic-auth-form></app-dynamic-auth-form>
+      </div>
+    `,
+  props: {
+    onChecked: action('onChecked'),
+    onSubmit: (e: User) => {
+      console.log('Login', e);
+    },
+  },
+});
