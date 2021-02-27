c6e8b508e70fc034b73009248a4a4ec4802bfe91
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-27 16:24:03 -0500

    add - sec19-using-ngtemplateoutlet-with-context

---
+++ src/app/sec19-using-ngtemplateoutlet-with-context/example.stories.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { moduleMetadata } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import { ExampleComponent } from './example.component';
+
+export default {
+  title: 'Ultimate Angular/19 Using ngTemplateOutlet With Context',
+  component: ExampleComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [ExampleComponent],
+      imports: [CommonModule],
+    }),
+  ],
+} as Meta;
+
+export const Example: Story = () => ({
+  template: `
+  <div class="app-root" style="padding: 15px;">
+    <app-example></app-example>
+  </div>
+`,
+});
---
+++ src/app/sec19-using-ngtemplateoutlet-with-context/sec19-using-ngtemplateoutlet-with-context.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExampleComponent } from './example.component';
+
+
+
+@NgModule({
+  declarations: [ExampleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class Sec19UsingNgtemplateoutletWithContextModule { }
