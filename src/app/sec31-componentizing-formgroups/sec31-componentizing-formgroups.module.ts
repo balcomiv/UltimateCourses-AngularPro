77491ab77ebd4aca34daf5aae6577c94cf68ab2b
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-03-08 07:56:41 -0500

    update - sec31 work

--- src/app/sec31-componentizing-formgroups/sec31-componentizing-formgroups.module.ts
+++ src/app/sec31-componentizing-formgroups/sec31-componentizing-formgroups.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
 import { StockProductsComponent } from './components/stock-products/stock-products.component';
-import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
+import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 
 @NgModule({
   declarations: [
@@ -14,5 +14,6 @@ import { StockSelectorComponent } from './components/stock-selector/stock-select
     StockSelectorComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule],
+  exports: [StockInventoryComponent],
 })
 export class Sec31ComponentizingFormgroupsModule {}
--- src/app/sec31-componentizing-formgroups/stock-inventory.stories.ts
+++ src/app/sec31-componentizing-formgroups/stock-inventory.stories.ts
@@ -1,21 +1,21 @@
-import { ReactiveFormsModule } from '@angular/forms';
 import { moduleMetadata } from '@storybook/angular';
 import { Meta, Story } from '@storybook/angular/types-6-0';
 import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
+import { Sec31ComponentizingFormgroupsModule } from './sec31-componentizing-formgroups.module';
 
 export default {
   title: 'Ultimate Angular Pro/Reactive Forms/31 Componentizing Form Groups',
   component: StockInventoryComponent,
   decorators: [
     moduleMetadata({
-      declarations: [StockInventoryComponent],
-      imports: [ReactiveFormsModule],
+      declarations: [],
+      imports: [Sec31ComponentizingFormgroupsModule],
     }),
   ],
 } as Meta;
 
 export const WithTemplate: Story = () => ({
   template: `
-   <app-stock-inventory></app-stock-inventory>
+    <app-stock-inventory></app-stock-inventory>
 `,
 });
