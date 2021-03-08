dd0c49dff689d670a39b37c2bc1d4b5487db5d0c
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-03-08 04:47:13 -0500

    update: sec31; add ui/components for component split

--- src/app/sec31-componentizing-formgroups/sec31-componentizing-formgroups.module.ts
+++ src/app/sec31-componentizing-formgroups/sec31-componentizing-formgroups.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StockInventoryComponent } from './stock-inventory/stock-inventory.component';
+import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
+import { StockProductsComponent } from './components/stock-products/stock-products.component';
 
 @NgModule({
-  declarations: [StockInventoryComponent],
+  declarations: [StockInventoryComponent, StockBranchComponent, StockProductsComponent],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class Sec31ComponentizingFormgroupsModule {}
