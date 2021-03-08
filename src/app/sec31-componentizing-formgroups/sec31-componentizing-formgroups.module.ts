f3b5c31293917969954342ce0d70c99109ff97e1
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-03-08 04:53:51 -0500

    update - sec31; move/update container component

--- src/app/sec31-componentizing-formgroups/sec31-componentizing-formgroups.module.ts
+++ src/app/sec31-componentizing-formgroups/sec31-componentizing-formgroups.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { StockInventoryComponent } from './stock-inventory/stock-inventory.component';
 import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
 import { StockProductsComponent } from './components/stock-products/stock-products.component';
+import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 
 @NgModule({
-  declarations: [StockInventoryComponent, StockBranchComponent, StockProductsComponent],
+  declarations: [
+    StockInventoryComponent,
+    StockBranchComponent,
+    StockProductsComponent,
+  ],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class Sec31ComponentizingFormgroupsModule {}
--- src/app/sec31-componentizing-formgroups/stock-inventory.stories.ts
+++ src/app/sec31-componentizing-formgroups/stock-inventory.stories.ts
@@ -1,7 +1,7 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { moduleMetadata } from '@storybook/angular';
 import { Meta, Story } from '@storybook/angular/types-6-0';
-import { StockInventoryComponent } from './stock-inventory/stock-inventory.component';
+import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 
 export default {
   title: 'Ultimate Angular Pro/Reactive Forms/31 Componentizing Form Groups',
